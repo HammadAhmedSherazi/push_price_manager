aff244df4f7f88abdd5bf9c05dc3e30f768aa7c1
Hammad Ahmed <46845004+HammadAhmedSherazi@users.noreply.github.com>
2025-10-15 17:42:05 +0500

    scan functionality to similar IOS device

--- lib/views/manager/listing_request/scan_view.dart
+++ lib/views/manager/listing_request/scan_view.dart
@@ -27,7 +27,7 @@ class _ScanViewState extends State<ScanView> {
       
       child: ListView(
         padding: EdgeInsets.all(AppTheme.horizontalPadding), children: [
-           Platform.isIOS ?
+          //  Platform.isIOS ?
           Consumer(
             builder: (context, ref, child) {
               final providerVM = ref.watch(productProvider);
@@ -65,43 +65,44 @@ class _ScanViewState extends State<ScanView> {
                 ),
               );
             }
-          ): 
-           ClipRRect(
-            borderRadius: BorderRadius.circular(20.r),
-             child: Consumer(
-               builder: (context, ref, child) {
-                final providerVM = ref.watch(productProvider);
-                 return SizedBox(
-                      width: 200,
-                      height: context.screenheight * 0.42,
-                      child: SimpleBarcodeScanner(
-                        // scanType: ScanType.qr,
-                        scaleHeight: 200,
-                        delayMillis: 2000,
-                        scaleWidth: 500,
-                        continuous: true,
+          ),
+          // : 
+          //  ClipRRect(
+          //   borderRadius: BorderRadius.circular(20.r),
+          //    child: Consumer(
+          //      builder: (context, ref, child) {
+          //       final providerVM = ref.watch(productProvider);
+          //        return SizedBox(
+          //             width: 200,
+          //             height: context.screenheight * 0.42,
+          //             child: SimpleBarcodeScanner(
+          //               // scanType: ScanType.qr,
+          //               scaleHeight: 200,
+          //               delayMillis: 2000,
+          //               scaleWidth: 500,
+          //               continuous: true,
     
-                        onScanned: (code) {
-                          print(code);
+          //               onScanned: (code) {
+          //                 print(code);
                           
-                          if(providerVM.getProductReponse.status != Status.loading){
-                            Helper.showFullScreenLoader(context);
-                            ref.read(productProvider.notifier).getProductData(code);
-                          }
+          //                 if(providerVM.getProductReponse.status != Status.loading){
+          //                   Helper.showFullScreenLoader(context);
+          //                   ref.read(productProvider.notifier).getProductData(code);
+          //                 }
                          
-                          // setState(() {
-                          //   result = code;
-                          // });
-                        },
+          //                 // setState(() {
+          //                 //   result = code;
+          //                 // });
+          //               },
                         
                         
-                        onBarcodeViewCreated: (BarcodeViewController controller) {
-                          this.controller = controller;
-                        },
-                      ));
-               }
-             ),
-           ),
+          //               onBarcodeViewCreated: (BarcodeViewController controller) {
+          //                 this.controller = controller;
+          //               },
+          //             ));
+          //      }
+          //    ),
+          //  ),
           Padding(padding: EdgeInsets.symmetric(
             vertical: 20.r,
             horizontal: 50.r
